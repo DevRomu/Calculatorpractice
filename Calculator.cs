d30d0dd177e89e1afb4396ce935696c49808370d
Ro Orina <orinarhomill@gmail.com>
2022-01-18 11:14:39 -0600

    Regularly Updating this mini-project of mines.
    Signed-off-by: Ro Orina <orinarhomill@gmail.com>

---
+++ Calculator.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/********************************
+*Description: Making a calculator from scratch
+*Date:2022-18-01
+*Version:1.0
+*************************************/
+
+namespace Orina.Rhomill.Practice
+{
+   
+    ///
+    ///This is the main class for the calculator. 
+    ///
+    internal class Calculator()
+    {
+    decimal = input1; 
+    decimal = input2;
+    string  = operation;
+
+
+        Console.ReadLine();
+
+        DecimalFormat twoDecimals = new DecimalFormat("#,###,###.##")
+    }
+}
